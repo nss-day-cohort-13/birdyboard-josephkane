eb437483253e8b075070b0453e4d1fb51b41758a
Joseph Kane <josephckane@gmail.com>
2016-08-03 14:38:41 -0500

    revert to original chirp structure

--- chirps.py
+++ chirps.py
@@ -1,8 +1,20 @@
 class Chirp:
 
-	def __init__(self, message, author, chirp_id, private=False, recipient=None):
+	def __init__(self, message, author, chirp_id):
 		self.message = message
 		self.author = author
 		self.chirp_id = chirp_id
-		self.private = private
+
+class PublicChirp(Chirp):
+
+	def __init__(self, message, author, chirp_id):
+		super().__init__(message, author, chirp_id)
+		self.permission = "public"
+
+class PrivateChirp(Chirp):
+
+	def __init__(self, message, author, recipient, chirp_id):
+		super().__init__(message, author, chirp_id)
+		self.permission = "private"
 		self.recipient = recipient
+
